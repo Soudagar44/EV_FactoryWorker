b3c3968b56e968f74cbc86dc8e9fb24d2d4142bb
icon <srramane6@gmail.com>
2021-10-10 08:59:40 +0530

    Resolve conflict

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome React For Edit <code>src/App.js</code> and save to reload.
+          Welcome React For Edit Siddu <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"

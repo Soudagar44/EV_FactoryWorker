2f17e0b6b8d3baf7cf805c3c9151d0bc9c50bb75
icon <srramane6@gmail.com>
2021-11-18 18:04:20 +0530

    Added Inspection

---
+++ src/components/Issues/Page.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import IForm from "./IForm";
+import PageHeader from "../Inspection/Others/PageHeader";
+import PeopleOutlineTwoToneIcon from '@material-ui/icons/PeopleOutlineTwoTone';
+import { Paper,makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    pageContent: {
+        margin: theme.spacing(5),
+        padding: theme.spacing(3)
+    }
+}))
+
+export default function Page() {
+
+    const classes = useStyles();
+
+    return (
+        <>
+            <PageHeader
+                title="Issues"
+                subTitle="In factory"
+                icon={<PeopleOutlineTwoToneIcon fontSize="large" />}
+            />
+            <Paper className={classes.pageContent}>
+                <IForm />
+            </Paper>
+        </>
+    )
+}
